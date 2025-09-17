17188a52f79a1dbe42b9275d4c82ee1dc7bb491a
mgonpor <miguelgp2005@gmail.com>
2025-09-17 12:13:44 +0200

    dfddd

---
+++ Unidad 1/script.js
@@ -0,0 +1,22 @@
+// act1.html
+function comprobador_edad() {
+    var edad = window.prompt("Introduce tu edad");
+
+    console.log("Tu edad es: " + edad);
+    
+    if (edad >= 18) {
+        console.log("Eres mayor de edad");
+    } else {
+        console.log("Eres menor de edad");
+    }
+}
+
+// act2.html
+function pintar_ruta() {
+    console.log("El acceso a la ruta C:\\\\usuario\\ tarda 1'23\", algo considerado \"lento\" en la actualidad");
+}
+
+// act3.html
+function pintar_ruta_partida() {
+    
+}
