65a093a05183dce7f68b0c2a1c6a9c0ea535a5ff
mgonpor <miguelgp2005@gmail.com>
2025-11-04 13:22:23 +0100

    ejercicio1 parte 3 boletin 2

--- Unidad 2/Boletin2/scriptNodos.js
+++ Unidad 2/Boletin2/scriptNodos.js
@@ -17,3 +17,68 @@ window.onload = function () {
 ------------------------------------------------------------------------
 */
 // Ejercicio 1
+function ejercicio1() {
+    var opcion = document.getElementById("option").value;
+    var lista = document.getElementById("lista");
+    var parrafo = document.getElementById("result");
+    switch (opcion) {
+        case "a":
+            parrafo.textContent = "El número de elementos es " + lista.childElementCount;
+            break;
+        case "b":
+            parrafo.textContent = "Primero: " + lista.firstElementChild.textContent + "\n -- Último: " + lista.lastElementChild.textContent;
+            break;
+        case "c":
+            var nc = Math.abs(Number(prompt("Número: ")));
+            if (lista.children.length >= nc) {
+                lista.appendChild(document.createElement("li")).textContent = lista.children.item(nc - 1).textContent;
+                parrafo.textContent = "Añadido elemento " + nc;
+            }
+            else {
+                parrafo.textContent = "No existe el nodo " + nc;
+            }
+            break;
+        case "d":
+            var nd = Math.abs(Number(prompt("Número: ")));
+            if (lista.children.length >= nd) {
+                lista.children.item(nd - 1).textContent = prompt("Nuevo contenido: ");
+                parrafo.textContent = "Modificado elemento " + nd;
+            }
+            else {
+                parrafo.textContent = "No existe el nodo " + nd;
+            }
+            break;
+        case "e":
+            for (var i = 0; i < lista.children.length; i++) {
+                parrafo.textContent += (i + 1) + "." + lista.children.item(i).textContent + " - ";
+            }
+            break;
+        case "f":
+            lista.appendChild(document.createElement("li")).textContent = prompt("Contenido: ");
+            parrafo.textContent = "Añadido nuevo elemento.";
+            break;
+        case "g":
+            var ng = Math.abs(Number(prompt("Elige un elemento a eliminar")));
+            if (lista.children.length >= ng) {
+                lista.removeChild(lista.children.item(ng - 1));
+                parrafo.textContent = "Elemento " + ng + " eliminado";
+            }
+            else {
+                parrafo.textContent = "No existe el nodo " + ng;
+            }
+            break;
+        case "h":
+            var elementos = [].slice.call(lista.children);
+            elementos.sort(function (a, b) { return a.textContent.localeCompare(b.textContent); });
+            lista.innerHTML = "";
+            for (var i = 0; i < elementos.length; i++) {
+                lista.appendChild(elementos[i]);
+            }
+            parrafo.textContent = "Lista ordenada alfabéticamente.";
+            break;
+        default:
+            parrafo.textContent = "Elige una opción válida";
+            break;
+    }
+}
+// Ejercicio 2
--- Unidad 2/Boletin2/scriptNodos.ts
+++ Unidad 2/Boletin2/scriptNodos.ts
@@ -21,3 +21,67 @@ window.onload = (): void => {
 */
 
 // Ejercicio 1
+function ejercicio1(): void {
+    let opcion: string = (document.getElementById("option") as HTMLInputElement).value;
+    let lista: HTMLOListElement = document.getElementById("lista") as HTMLOListElement;
+    let parrafo: HTMLParagraphElement = document.getElementById("result") as HTMLParagraphElement;
+
+    switch (opcion) {
+        case "a":
+            parrafo.textContent = "El número de elementos es " + lista.childElementCount;
+            break;
+        case "b":
+            parrafo.textContent = "Primero: " + lista.firstElementChild.textContent + "\n -- Último: " + lista.lastElementChild.textContent;
+            break;
+        case "c":
+            let nc: number = Math.abs(Number(prompt("Número: ")));
+            if(lista.children.length >= nc){
+                lista.appendChild(document.createElement("li")).textContent = lista.children.item(nc-1).textContent;
+                parrafo.textContent = "Añadido elemento " + nc;
+            }else{
+                parrafo.textContent = "No existe el nodo " + nc;
+            }
+            break;
+        case "d":
+            let nd: number = Math.abs(Number(prompt("Número: ")));
+            if(lista.children.length >= nd){
+                lista.children.item(nd-1).textContent = prompt("Nuevo contenido: ");
+                parrafo.textContent = "Modificado elemento " + nd;
+            }else{
+                parrafo.textContent = "No existe el nodo " + nd;
+            }
+            break;
+        case "e":
+            for(let i = 0; i < lista.children.length; i++){
+                parrafo.textContent += (i+1) + "." + lista.children.item(i).textContent + " - ";
+            }
+            break;
+        case "f":
+            lista.appendChild(document.createElement("li")).textContent = prompt("Contenido: ");
+            parrafo.textContent = "Añadido nuevo elemento.";
+            break;
+        case "g":
+            let ng: number = Math.abs(Number(prompt("Elige un elemento a eliminar")));
+            if(lista.children.length >= ng){
+                lista.removeChild(lista.children.item(ng-1));
+                parrafo.textContent = "Elemento " + ng + " eliminado";
+            }else{
+                parrafo.textContent = "No existe el nodo " + ng;
+            }
+            break;
+        case "h":
+            let elementos: HTMLLIElement[] = [].slice.call(lista.children) as HTMLLIElement[];
+            elementos.sort((a: HTMLLIElement, b: HTMLLIElement): number => a.textContent.localeCompare(b.textContent));
+            lista.innerHTML = "";
+            for(let i = 0; i < elementos.length; i++){
+                lista.appendChild(elementos[i]);
+            }
+            parrafo.textContent = "Lista ordenada alfabéticamente.";
+            break;
+        default:
+            parrafo.textContent = "Elige una opción válida";
+            break;
+    }
+}
+
+// Ejercicio 2
