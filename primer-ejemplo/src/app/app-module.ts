c3df8d59fed44f73aa0bc2e2a508fe6e5c07c3fc
mgonpor <miguelgp2005@gmail.com>
2025-11-19 11:16:08 +0100

    trastear con angular

---
+++ primer-ejemplo/src/app/app-module.ts
@@ -0,0 +1,27 @@
+import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { BrowserModule, provideClientHydration, withEventReplay } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing-module';
+import { App } from './app';
+import {Header} from "../components/header/header";
+import {Footer} from '../components/footer/footer';
+import {Home} from '../components/home/home';
+
+@NgModule({
+  declarations: [
+    App,
+    Header,
+    Footer,
+    Home
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideClientHydration(withEventReplay())
+  ],
+  bootstrap: [App]
+})
+export class AppModule { }
