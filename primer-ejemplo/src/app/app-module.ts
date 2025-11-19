1910bf41e4ec2f43cdeb7a1688c6c322f75156a5
mgonpor <miguelgp2005@gmail.com>
2025-11-19 12:51:52 +0100

    contacto y servicios.

--- primer-ejemplo/src/app/app-module.ts
+++ primer-ejemplo/src/app/app-module.ts
@@ -6,17 +6,25 @@ import { App } from './app';
 import {Header} from "../components/header/header";
 import {Footer} from '../components/footer/footer';
 import {Home} from '../components/home/home';
+import {Servicios} from '../components/servicios/servicios';
+import {Contacto} from '../components/contacto/contacto';
+import {NgOptimizedImage} from '@angular/common';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
     App,
     Header,
     Footer,
-    Home
+    Home,
+    Servicios,
+    Contacto
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgOptimizedImage,
+    FormsModule
   ],
   providers: [
     provideBrowserGlobalErrorListeners(),
--- primer-ejemplo/src/app/app-routing-module.ts
+++ primer-ejemplo/src/app/app-routing-module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {Home} from '../components/home/home';
+import {Servicios} from '../components/servicios/servicios';
+import {Contacto} from '../components/contacto/contacto';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", redirectTo: "home", pathMatch: "full" },
+  {path: "home", component: Home},
+  {path: "services", component: Servicios},
+  {path: "contact", component: Contacto},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
