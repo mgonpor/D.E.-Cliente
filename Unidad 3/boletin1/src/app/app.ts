4a728e7cf399ff330a766dd49167e615e66a0e24
mgonpor <miguelgp2005@gmail.com>
2025-11-27 13:20:41 +0100

    seguir actividades

--- Unidad 3/boletin1/src/app/app.ts
+++ Unidad 3/boletin1/src/app/app.ts
@@ -72,11 +72,87 @@ export class App {
     let index = this.frutas.length;
     for (let i = 0; i < this.frutas.length; i++) {
       let v = (this.frutas[i] > fruta) ? -1 : 1;
-      if (v == 1){
+      if (v == -1){
         index = i;
         break;
       }
     }
     this.frutas.splice(index, 0, fruta);
+    console.log(this.frutas);
   }
+
+  // Ejer 11
+  public diaSemana: Set<string> = new Set<string>();
+  public entreSemana: Set<string> = new Set<string>(["Lunes", "Martes", "Miércoles", "Jueves", "Viernes"]);
+  public finde: Set<string> = new Set<string>(["Viernes", "Sábado", "Domingo"]);
+
+  public rellena_semana(): void {
+    this.diaSemana.add("Lunes")
+      .add("Martes")
+      .add("Miércoles")
+      .add("Jueves")
+      .add("Viernes")
+      .add("Viernes")
+      .add("Viernes")
+      .add("Viernes")
+      .add("Sábado")
+      .add("Domingo")
+      .add("Sábado");
+  }
+
+  public unir_conjuntos(): void {
+    // el operador ... coje lo que haya en el interior del conjunto/array
+    this.diaSemana = new Set([...this.entreSemana, ...this.finde]);
+
+  }
+
+  public alumnos: Set<string> = new Set<string>();
+
+  public operar_conjuntos(opt: number): void {
+    switch (opt){
+      case 1:
+        this.add_to_set();
+        console.log(this.alumnos);
+        break;
+      case 2:
+        this.delete_from_set();
+        console.log(this.alumnos);
+        break;
+      case 3:
+        console.log("Xexu: " + this.find_in_set("Xexu"));
+        console.log("Juan: " + this.find_in_set("Juan"));
+        break;
+      case 4:
+        console.log("Tamaño: " + this.count_set());
+        break;
+    }
+  }
+
+  private add_to_set(): void {
+    this.alumnos.add("Pedro").add("Sara").add("Xexu").add("Maria").add("Juan");
+  }
+
+  private delete_from_set(): void {
+    this.alumnos.delete("Xexu");
+  }
+
+  private find_in_set(alumno: string): boolean {
+    return this.alumnos.has(alumno);
+  }
+
+  private count_set(): number {
+    return this.alumnos.size;
+  }
+
+  // Ejer 13
+  public lista: Array<any> = ["Pepe", "Maria", "Pepe", "Juan", "Maria", "Pepe"];
+
+  public eliminaDuplicados(l: Array<any>): Array<any> {
+    let s: Set<any> = new Set();
+    s.add([...l]);
+    let array: Array<any> = Array(...s);
+    console.log(array);
+    return array;
+  }
+
 }
