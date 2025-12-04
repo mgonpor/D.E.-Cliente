d32fc7f32dfe14d6f0b216fa911a106da12d3b40
Miguel González del Portillo <miguelgp2005@gmail.com>
2025-12-04 13:21:15 +0100

    alumno

---
+++ Unidad 3/boletin2/src/app/models/profesor.ts
@@ -0,0 +1,24 @@
+export class Profesor {
+
+  constructor(
+    private _dni: string,
+    private _nombre: string,
+    private _apellidos: string,
+    private _telefono: string,
+    private _salarioBruto: number
+  ) {}
+
+  // Getters y Setters
+  get dni(): string { return this._dni; };
+  get nombre(): string { return this._nombre; };
+  get apellidos(): string { return this._apellidos; };
+  get telefono(): string { return this._telefono; };
+  get salarioBruto(): number { return this._salarioBruto; };
+
+  set dni(dni: string) { this._dni = dni; };
+  set nombre(dni: string) { this._nombre = dni; };
+  set apellidos(dni: string) { this._apellidos = dni; };
+  set telefono(dni: string) { this._telefono = dni; };
+  set salarioBruto(salarioBruto: number) { this._salarioBruto = salarioBruto; };
+
+}
