4698ef81cd664e19d57f789d4c1279e3ae51fdce
mgonpor <miguelgp2005@gmail.com>
2026-02-13 10:27:01 +0100

    nueva columna

--- Unidad 4/kanban/src/app/app.routes.ts
+++ Unidad 4/kanban/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { Board } from './components/board/board';
 import { BoardDetail } from './components/board-detail/board-detail';
 import { BoardNew } from './components/board-new/board-new';
+import { BoardDetailNew } from './components/board-detail-new/board-detail-new';
 
 export const routes: Routes = [
     { path: '', component: Board },
     { path: 'tablero', component: Board },
     { path: 'tablero/nuevo', component: BoardNew },
-    { path: 'tablero/:id', component: BoardDetail }
+    { path: 'tablero/:id', component: BoardDetail },
+    { path: 'tablero/:id/columna-nueva', component: BoardDetailNew }
 ];
--- Unidad 4/kanban/src/app/components/board-detail-new/board-detail-new.ts
+++ Unidad 4/kanban/src/app/components/board-detail-new/board-detail-new.ts
@@ -1,25 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { Columna } from '../../models/columna';
 import { ColumnaService } from '../../services/columna-service';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { TableroService } from '../../services/tablero-service';
 
 @Component({
   selector: 'app-board-detail-new',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './board-detail-new.html',
   styleUrl: './board-detail-new.css',
 })
-export class BoardDetailNew {
+export class BoardDetailNew implements OnInit {
 
-  // todo: recibir el tablero
-  private nuevaColumna: Columna = new Columna(0, '', '', null);
+  public nuevaColumna!: Columna;
 
-  constructor(private columnaService: ColumnaService, private router: Router) { }
+  constructor(private columnaService: ColumnaService, private tableroService: TableroService,
+    private router: Router, private actRouter: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    const idTablero = Number(this.actRouter.snapshot.params['id']);
+    const tablero = this.tableroService.findById(idTablero);
+    if (tablero) {
+      this.nuevaColumna = new Columna(0, '', '', tablero);
+    }
+  }
 
   public crearColumna() {
     this.nuevaColumna.id = this.columnaService.autoIncrement();
     this.columnaService.save(this.nuevaColumna);
-    this.router.navigate(['/tablero/:id']);
+    this.router.navigate(['/tablero', this.nuevaColumna.tablero.id]);
   }
 
 }
